3f129a82f7a61c13c15815c83c5897ca225b738c
shubham kumar <shubhamfarainzi@gmail.com>
2026-06-30 21:13:27 +0530

    updated

--- html/one.js
+++ html/one.js
@@ -1,8 +1,13 @@
 
 
-// html -->
+// html --> hypertext markup language--> used to create structure of a web page
+
+// HyperText--> used to link two or more pages with each other
+
+// markupLanguage --> it format and display the text. it can turn text into images , videos, audios , etc.
 
 // ! --> generate basic html code (boilerplate code) --> shift + 1
-// dummy text --> lorem40
+// dummy text --> lorem40 --> generate dummy text of 40 words
 // text wrap --> alt + z 
-// 
+
+// tags --> 
